4038b5a83f50aaf2e7115629d9f76d1ec18ff3fb
Cillian <cillians@gmail.com>
2014-01-14 20:40:03 +0000

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pickpocket::Application.config.secret_key_base = '645c98b6c34867c23940ae57b41d7bca87031995edb48aa905df647d38f44632bc3d7883772ebe65ff2f858a3b31eb3f0e9f4580ce185646d4666247d6f344f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pickpocket::Application.config.session_store :cookie_store, key: '_pickpocket_session'
