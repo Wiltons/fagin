97142bbc0d4d3ca6ff992b5c2c0c7c6fe96af3be
Eugene Kenny <elkenny@gmail.com>
2014-08-23 21:23:24 +0100

    Remove unnecessary signed_in? check
    There is already a filter that makes sure a user is signed in, so this
    condition will never be false.

--- app/controllers/pushes_controller.rb
+++ app/controllers/pushes_controller.rb
@@ -3,24 +3,22 @@ class PushesController < ApplicationController
   before_action :signed_in_user
 
   def new
-    @push = current_user.pushes.build if signed_in?
+    @push = current_user.pushes.build
   end
 
   def create
-    if signed_in?
-      @push = current_user.pushes.create(push_params)
-      if @push.save
-        flash[:success] = "Rule created successfully"
-        if params[:commit] == "Save and Create New"
-          redirect_to push_path
-        else
-          @push.tag_articles unless params[:commit] == "Save"
-          redirect_to pushes_path
-        end
+    @push = current_user.pushes.create(push_params)
+    if @push.save
+      flash[:success] = "Rule created successfully"
+      if params[:commit] == "Save and Create New"
+        redirect_to push_path
       else
-        flash[:error] = "Errors exist"
-        render :new
+        @push.tag_articles unless params[:commit] == "Save"
+        redirect_to pushes_path
       end
+    else
+      flash[:error] = "Errors exist"
+      render :new
     end
   end
 
